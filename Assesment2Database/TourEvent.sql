708097fe11f900733efc85d36bf443655981b4c5
theeateroffood <theeateroffood@gmail.com>
2018-06-22 10:19:51 +1000

    TourEvent.sql added

---
+++ Assesment2Database/TourEvent.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[TourEvent]
+(
+	[TourEventId] INT NOT NULL CONSTRAINT PK_TourEvent PRIMARY KEY, 
+	[TourName] NVARCHAR(50) NOT NULL,
+    [EventMonth] NVARCHAR(50) NULL, 
+    [EventDay] NVARCHAR(50) NULL, 
+    [EventYear] NVARCHAR(4) NULL, 
+    [Fee] NVARCHAR(5) NULL
+	CONSTRAINT FK_TourEvent_Tour FOREIGN KEY ([TourName]) REFERENCES [Tour] ([TourName])
+)
